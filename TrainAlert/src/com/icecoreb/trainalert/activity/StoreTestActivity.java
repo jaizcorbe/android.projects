35adaf645506911072b8cf61f2e8c0eac76ea7be
jaizcorbe <jaizcorbe@jaizcorbe-PC>
2014-05-29 22:11:09 -0300

    cleanup

--- TrainAlert/src/com/icecoreb/trainalert/activity/StoreTestActivity.java
+++ TrainAlert/src/com/icecoreb/trainalert/activity/StoreTestActivity.java
@@ -1,13 +1,9 @@
 package com.icecoreb.trainalert.activity;
 
 import android.app.Activity;
-import android.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 
 import com.icecoreb.trainalert.R;
 
@@ -39,21 +35,5 @@ public class StoreTestActivity extends Activity {
 		return super.onOptionsItemSelected(item);
 	}
 
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_store_test,
-					container, false);
-			return rootView;
-		}
-	}
 
 }
